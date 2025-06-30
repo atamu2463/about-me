bfef4405ead30d7eb33e1938b2c666add2ce9e1d
asa-cording <asahiwork.cording@gmail.com>
2025-07-01 01:29:15 +0900

    自動デプロイ設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
  const Page = () => {
-  const key = process.env.TEST_SECRET_KEY
   return (
     <div className="flex h-screen items-center justify-center">
       <h1 className="text-4xl font-bold">Hello World!</h1>
-      <p className="mt-4 text-lg">環境変数の値：{key}</p>
     </div>
   );
 }
